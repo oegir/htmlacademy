79708c02b800cdb5ff6f424248a95200b985e543
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2021-03-03 13:20:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+// Активация утверждений и отключение вывода ошибок
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+assert_options(ASSERT_QUIET_EVAL, 1);
+// Создание обработчика
+function my_assert_handler($file, $line, $code, $desc = null) {
+    print_r($desc);
+}
+// Подключение callback-функции
+assert_options(ASSERT_CALLBACK, 'my_assert_handler');
 
 require 'src'.DIRECTORY_SEPARATOR.'Task.php'; 
 
 $task = new Task(1, 2);
 
-if($task->getNextStatus(Task::ACTION_APPROVE, Task::CUSTOMER_ROLE) == Task::STATUS_IN_WORK) {
-    echo 'Следующий статус: ' . $task->getNextStatus(Task::ACTION_APPROVE, Task::CUSTOMER_ROLE);
-}
-
-echo '<hr/>';
-
-echo 'Доступные действия для заказчика:';
-echo '<pre>';
-var_dump($task->getAvailableCustomerActions(Task::STATUS_NEW));
-echo '</pre>';
+assert($task->getNextStatus(Task::ACTION_APPROVE, Task::CUSTOMER_ROLE) == Task::STATUS_IN_WORK, 'Approve action failed');
