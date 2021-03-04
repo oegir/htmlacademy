cb8483a220d60a3bf4607eb62fc1f3e374cdff7c
Artem2J <2j.artem@gmail.com>
2021-03-04 23:43:21 +0300

    Заполнение лотов названием, разделом, ценой и изображением товаров

--- index.php
+++ index.php
@@ -62,17 +62,18 @@ $items_arr =   [['name' => '2014 Rossignol District Snowboard', 'category' => '�
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?foreach ($items_arr as $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$item['url']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$item['category']; ?>></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$item['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -80,6 +81,7 @@ $items_arr =   [['name' => '2014 Rossignol District Snowboard', 'category' => '�
                     </div>
                 </div>
             </li>
+            <?endforeach; ?>
         </ul>
     </section>
 </main>
