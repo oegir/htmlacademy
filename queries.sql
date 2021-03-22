325c831ec7c29f0f00a130345a83366eea56ab03
Artem2J <2j.artem@gmail.com>
2021-03-22 06:41:26 +0300

    Заполняем категории и пользователей

---
+++ queries.sql
@@ -0,0 +1,30 @@
+INSERT INTO category SET name = 'Доски и лыжи',
+                         code = 'boardsandskis';
+
+INSERT INTO category SET name = 'Крепления',
+                         code = 'mounts';
+
+INSERT INTO category SET name = 'Ботинки',
+                         code = 'boots';
+
+INSERT INTO category SET name = 'Одежда',
+                         code = 'clothing';
+
+INSERT INTO category SET name = 'Инструменты',
+                         code = 'tools';
+
+INSERT INTO category SET name = 'Доски и лыжи',
+                         code = 'other';
+
+
+INSERT INTO user SET registration_date = '2021-03-20 10:00:00',
+                     email = '1@eml.ru',
+                     name = 'Alexandra',
+                     password = 'password',
+                     contacts = 'contacts';
+
+INSERT INTO user SET registration_date = '2021-03-20 10:30:00',
+                     email = '2@eml.ru',
+                     name = 'Kristal',
+                     password = 'password',
+                     contacts = 'contacts';
