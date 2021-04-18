3a419b1cd8245de699774b62c9bf4f9f38953b2b
Artem2J <2j.artem@gmail.com>
2021-04-18 23:00:33 +0300

    Сохранение данных в форме

--- registration.php
+++ registration.php
@@ -4,17 +4,26 @@ require_once('db_connection.php');
 require_once('service_functions.php');
 
 $con = db_connect();
-$user_name = 'Artem2J'; // укажите здесь ваше имя
-$incoming_data = ['lot-name' => '', 'category' => '', 'message' => '',
-                  'lot-rate' => 0, 'lot-step' => 0, 'lot-date' => ''];
-
+$incoming_data = ['email' => '', 'password' => '', 'name' => '', 'message' => ''];
 $form_errors = [];
+
+if(isset($_POST['submit'])){
+    $incoming_data = $_POST;
+    $form_errors = checkRegistrationErrors($con, $incoming_data);
+}
 
 $categories_arr = [];
 $categories_arr = getCategories($con);
 
-$page_content = include_template('sign-up.php', ['categories_arr' => $categories_arr/*, 'incoming_data' => $incoming_data, 'form_errors' => $form_errors*/]);
+$page_content = include_template('sign-up.php', ['categories_arr' => $categories_arr, 'incoming_data' => $incoming_data/*, 'form_errors' => $form_errors*/]);
 
 $layout_content = include_template('layout.php', ['is_auth' => 0 ,'categories_arr' => $categories_arr, 'content' => $page_content ,'title' => 'Регистрация']);
 
 print($layout_content);
+
+function checkRegistrationErrors($con, $data): array{
+    $result = [];
+
+
+    return $result;
+}
