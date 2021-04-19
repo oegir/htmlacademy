4b61b52fe187260a7e19133e97c8412c93e3c21e
Artem2J <2j.artem@gmail.com>
2021-04-19 16:39:21 +0300

    Проверка уникальности email

--- registration.php
+++ registration.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])){
 $categories_arr = [];
 $categories_arr = getCategories($con);
 
-$page_content = include_template('sign-up.php', ['categories_arr' => $categories_arr, 'incoming_data' => $incoming_data/*, 'form_errors' => $form_errors*/]);
+$page_content = include_template('sign-up.php', ['categories_arr' => $categories_arr, 'incoming_data' => $incoming_data, 'form_errors' => $form_errors]);
 
 $layout_content = include_template('layout.php', ['is_auth' => 0 ,'categories_arr' => $categories_arr, 'content' => $page_content ,'title' => 'Регистрация']);
 
@@ -23,7 +23,18 @@ print($layout_content);
 
 function checkRegistrationErrors($con, $data): array{
     $result = [];
+    $result['email'] = checkEmail($con, $data['email']);
 
+    return $result;
+}
 
+function checkEmail($con, $email): string{
+    $result = '';
+    $sql = "SELECT * FROM user WHERE email = ?";
+    $stmt = db_get_prepare_stmt($con, $sql, [$email]);
+    mysqli_stmt_execute($stmt);
+    $res = mysqli_stmt_get_result($stmt);
+    $result = mysqli_num_rows($res) == 0 ? '': 'Указаный e-mail уже используется'; 
+    
     return $result;
 }
--- templates/sign-up.php
+++ templates/sign-up.php
@@ -11,22 +11,22 @@
     <form class="form container form--invalid" action="registration.php" method="post" autocomplete="off"> <!-- form
     --invalid -->
       <h2>Регистрация нового аккаунта</h2>
-      <div class="form__item"> <!-- form__item--invalid -->
+      <div class="form__item <?php if(isset($form_errors['email'])):?><?=$form_errors['email'];?><?php endif; ?>">
         <label for="email">E-mail <sup>*</sup></label>
         <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?=xss_protection($incoming_data['email']); ?>">
         <span class="form__error">Введите e-mail</span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?php if(isset($form_errors['password'])):?><?=$form_errors['password'];?><?php endif; ?>">
         <label for="password">Пароль <sup>*</sup></label>
         <input id="password" type="password" name="password" placeholder="Введите пароль" value="<?=xss_protection($incoming_data['password']); ?>">
         <span class="form__error">Введите пароль</span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?php if(isset($form_errors['name'])):?><?=$form_errors['name'];?><?php endif; ?>">
         <label for="name">Имя <sup>*</sup></label>
         <input id="name" type="text" name="name" placeholder="Введите имя" value="<?=xss_protection($incoming_data['name']); ?>">
         <span class="form__error">Введите имя</span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?php if(isset($form_errors['message'])):?><?=$form_errors['message'];?><?php endif; ?>">
         <label for="message">Контактные данные <sup>*</sup></label>
         <textarea id="message" name="message" placeholder="Напишите как с вами связаться"><?=xss_protection($incoming_data['message']); ?></textarea>
         <span class="form__error">Напишите как с вами связаться</span>
