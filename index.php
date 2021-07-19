9ba8da30101f685ff084b755d76c06113812fbdd
Dzoter <Andry94@list.ru>
2021-07-19 12:06:19 +0400

    Исправил и добавил условие для целой секции <nav>

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $user_name = 'Андрей'; // укажите здесь ваше имя
         </form>
         <div class="header__nav-wrapper">
             <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
-
+            <?php if ($is_auth == 1): ?>
             <nav class="header__nav">
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
@@ -70,9 +70,8 @@ $user_name = 'Андрей'; // укажите здесь ваше имя
                             <div class="header__profile-name">
                                 <span>
                                     <!--здесь должно быть имя пользователя-->
-                                    <? if ($is_auth ==1) {
-                                        print $user_name;
-                                    }?>
+                                    <?= $user_name ?>
+
                                 </span>
                                 <svg class="header__link-arrow" width="10" height="6">
                                     <use xlink:href="#icon-arrow-right-ad"></use>
@@ -114,10 +113,10 @@ $user_name = 'Андрей'; // укажите здесь ваше имя
                     </li>
                 </ul>
             </nav>
+            <?php endif; ?>
         </div>
     </div>
 </header>
-
 <section class="page__main page__main--popular">
     <div class="container">
         <h1 class="page__title page__title--popular">Популярное</h1>
