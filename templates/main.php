adbbda4b4156fb5a1cdc696ba014db0ab5650760
Olga Morozova <morozova31@gmail.com>
2021-07-28 20:13:05 +0300

    module3-task1

---
+++ templates/main.php
@@ -0,0 +1,49 @@
+
+    <section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
+            горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <!--заполните этот список из массива категорий-->
+            <?php
+                        $index = 0;
+                        $num = count($categories);
+                        while ($index < $num): ?>
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="pages/all-lots.html"><?=$categories[$index];?></a>
+            </li>
+            <?php $index++; ?>
+            <?php endwhile; ?>
+
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <!--заполните этот список из массива с товарами-->
+            <?php foreach ($items as $key => $val): ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?=$val['img']; ?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=htmlspecialchars($val['category']); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($val['title']); ?></a>
+                    </h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount"><?=htmlspecialchars($val['price']); ?></span>
+                            <span class="lot__cost"><?=auction_price(htmlspecialchars($val['price'])); ?></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            12:23
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+
