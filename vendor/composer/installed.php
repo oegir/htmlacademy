7b644c59bf0078fda84f6640eafeffdff3e08042
sovius23 <sovius5@mail.ru>
2021-07-28 18:36:48 +0300

    Добавление наставника

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'asus/1806383-task-force-1',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'asus/1806383-task-force-1' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
