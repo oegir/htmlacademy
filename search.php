8422d97e5ad81d3111a5130c3cb05cbcc943e049
Artem2J <2j.artem@gmail.com>
2021-09-03 16:49:40 +0300

    Получение массива данных по поисковому запросу

--- search.php
+++ search.php
@@ -4,12 +4,21 @@ require_once('helpers.php');
 require_once('db_connection.php');
 require_once('service_functions.php');
 
+if (!isset($_GET['search'])){
+    header('Location: pages/404.html');
+    die();
+}
+
 $categories_arr = [];
 $items_arr = [];
 
 $con = db_connect();
 
+$rqt = trim($_GET['search']);
+
 $user_name = getUserNameById($con, sess_get_user_id());
+
+$items_arr = getSearchItems($con, $rqt);
 
 $categories_arr = getCategories($con);
 
@@ -18,3 +27,27 @@ $page_content = include_template('search_tmp.php', ['categories_arr' => $categor
 $layout_content = include_template('layout.php', ['user_name' => $user_name, 'categories_arr' => $categories_arr, 'content' => $page_content ,'title' => 'Главная']);
 
 print($layout_content);
+
+function getSearchItems(mysqli $con, string $rqt) : array{
+    $sql = "SELECT    
+    i.id id, i.name, c.name category, IFNULL(b.price,start_price) price, img_path url, completion_date expiry_date
+        FROM  item i
+    LEFT JOIN category c on c.id = i.category_id
+    LEFT JOIN
+    (SELECT
+        item_id, MAX(price) price
+    FROM bid b2
+    GROUP BY item_id) b ON i.id = b.item_id
+    WHERE MATCH (i.name, i.description) AGAINST(?) AND i.winner_id IS NULL 
+    ORDER BY date DESC";
+    $items = [];
+    $stmt = db_get_prepare_stmt($con, $sql, [$rqt]);
+    mysqli_stmt_execute($stmt);
+    $res = mysqli_stmt_get_result($stmt);
+    
+    if ($res && $row = $res->fetch_assoc()){
+        $items[] = $row;
+    }
+    
+    return $items;
+}
