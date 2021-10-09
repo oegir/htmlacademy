c0558540191d6b9cc9c5fceafbad794022a88ca9
oegir <oegir@oegir-Aspire-A517-51G>
2021-10-09 17:58:11 +0300

    Repire and refactoring

---
+++ src/request.php
@@ -0,0 +1,36 @@
+<?php
+
+
+/**
+ * Валидация формы ГЕТ для строки
+ * @param string $param ключ параметра запроса
+ * @param string|null $default Значение по умолчанию
+ * @return string|null Возвращает параметр запроса в формате строки
+ */
+function request_retriveGetString(string $param, ?string $default): ?string
+{
+    {
+        $result = ($_GET[$param] ?? $default);
+        if (is_string($result)) {
+            return (string)$result;
+        }
+        
+        return $default;
+    }
+}
+
+/**
+ * Обеспечиваем безопасность от sql инъекций для чисел
+ * @param string $param Ключ массива $_GET
+ * @param int $default Значение по умолчанию
+ * @return int строго возвращает число
+ */
+function request_retriveGetInt(string $param, ?int $default): ?int
+{
+    $result = ($_GET[$param] ?? $default);
+    if (is_numeric($result)) {
+        return (int)$result;
+    }
+    
+    return $default;
+}
