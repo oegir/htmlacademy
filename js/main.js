cf1ed063a924a24672704301b667685c844ae8e0
Keks <keks@htmlacademy.ru>
2021-10-19 15:43:58 +0300

    :cat:

--- js/main.js
+++ js/main.js
@@ -1,69 +1,73 @@
-var openModalLinks = document.getElementsByClassName("open-modal");
-var closeModalLinks = document.getElementsByClassName("form-modal-close");
-var overlay = document.getElementsByClassName("overlay")[0];
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.pop-up');
+const imgPreviewElement = document.querySelector('.avatar-preview');
 
-for (var i = 0; i < openModalLinks.length; i++) {
-  var modalLink = openModalLinks[i];
+const actionButtons = document.querySelectorAll('.action-btn');
 
-  modalLink.addEventListener("click", function (event) {
-    var modalId = event.currentTarget.getAttribute("data-for");
+actionButtons.forEach(function (el) {
+    el.addEventListener('click', function (evt) {
+        const modalType = evt.target.dataset.action;
+        const modal = document.querySelector('.pop-up--' + modalType);
+        modal.classList.remove('pop-up--close');
+        modal.classList.add('pop-up--open');
+        overlay.classList.add('db');
+    })
+});
 
-    var modal = document.getElementById(modalId);
-    modal.setAttribute("style", "display: block");
-    overlay.setAttribute("style", "display: block");
+const buttonsClose = document.querySelectorAll('.button--close');
 
-  });
+buttonsClose.forEach(function (el) {
+    el.addEventListener('click', function (evt) {
+        const modalOpen = document.querySelector('.pop-up--open');
+        modalOpen.classList.remove('pop-up--open');
+        modalOpen.classList.add('pop-up--close');
+        overlay.classList.remove('db');
+
+    })
+});
+
+let buttonInput = document.querySelector('#button-input');
+
+if (buttonInput) {
+    buttonInput.addEventListener('change', function (evt) {
+        const file = evt.target.files[0];
+        const fileName = file.name.toLowerCase();
+
+        const matches = FILE_TYPES.some(function (it) {
+            return fileName.endsWith(it);
+        });
+        if (matches) {
+            const reader = new FileReader();
+            reader.addEventListener('load', function () {
+                imgPreviewElement.src = reader.result;
+            });
+            reader.readAsDataURL(file);
+        }
+    });
 }
 
-function closeModal(event) {
-  var modal = event.currentTarget.parentElement;
+var starRating = document.querySelector(".active-stars");
 
-  modal.removeAttribute("style");
-  overlay.removeAttribute("style");
-}
+if (starRating) {
+    starRating.addEventListener("click", function(event) {
+        var stars = event.currentTarget.childNodes;
+        var rating = 0;
 
-for (var j = 0; j < closeModalLinks.length; j++) {
-  var closeModalLink = closeModalLinks[j];
+        for (var i = 0; i < stars.length; i++) {
+            var element = stars[i];
 
-  closeModalLink.addEventListener("click", closeModal)
-}
+            if (element.nodeName === "SPAN") {
+                element.className = "fill-star";
+                rating++;
+            }
 
-document.getElementById('close-modal').addEventListener("click", closeModal);
+            if (element === event.target) {
+                break;
+            }
+        }
 
-var starRating = document.getElementsByClassName("completion-form-star");
-if (starRating.length) {
-  starRating = starRating[0];
-
-  starRating.addEventListener("click", function(event) {
-    var stars = event.currentTarget.childNodes;
-    var rating = 0;
-
-    for (var i = 0; i < stars.length; i++) {
-      var element = stars[i];
-
-      if (element.nodeName === "SPAN") {
-        element.className = "";
-        rating++;
-      }
-
-      if (element === event.target) {
-        break;
-      }
-    }
-
-    var inputField = document.getElementById("rating");
-    inputField.value = rating;
-  });
-}
-
-var cityDropdown = document.getElementsByClassName('town-select');
-
-if (cityDropdown.length) {
-  cityDropdown = cityDropdown[0];
-
-  cityDropdown.addEventListener('change', function(event) {
-    var selectedCity = event.target.value;
-
-    window.location = '/site/city?city=' + selectedCity;
-  });
+        var inputField = starRating.previousSibling;
+        inputField.value = rating;
+    });
 }
